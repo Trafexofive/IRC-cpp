29ce9d32fd576f98aa28914d2f886721d24fff01
Trafexofive <les5phenix@gmail.com>
2024-12-07 23:55:24 +0100

    latest

--- src/HandleEvents.cpp
+++ src/HandleEvents.cpp
@@ -10,69 +10,60 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"../inc/core_server.hpp"
+#include "../inc/core_server.hpp"
 
-void    CoreServer::WelcomeClient()
-{
-    int fd_c;
-    fd_c = accept(ServData._socket,NULL,NULL);
-      if (fd_c < 0)
-        { 
-            std::cout << "failure connencting client" << std::endl;
-            return ;
-        }
-     else
-        {
-             std::cout << "client accepted FD:" << fd_c << std::endl;
-        }
-        struct sockaddr_in client_addr;
-        socklen_t client_len = sizeof(client_addr);
-        getpeername(fd_c, (struct sockaddr*)&client_addr, &client_len);
-        clients[fd_c] = _client(fd_c,client_addr);
+void CoreServer::WelcomeClient() {
+  int fd_c;
+  fd_c = accept(ServData._socket, NULL, NULL);
+  if (fd_c < 0) {
+    std::cout << "failure connencting client" << std::endl;
+    return;
+  } else {
+    std::cout << "client accepted FD:" << fd_c << std::endl;
+  }
+  struct sockaddr_in client_addr;
+  socklen_t client_len = sizeof(client_addr);
+  getpeername(fd_c, (struct sockaddr *)&client_addr, &client_len);
+  clients[fd_c] = _client(fd_c, client_addr);
 
-        char host[NI_MAXHOST];
-        char service[NI_MAXSERV];
-        getnameinfo((struct sockaddr*)&clients[fd_c].get_info(), sizeof(client_addr), host, NI_MAXHOST, service, NI_MAXSERV, NI_NUMERICHOST | NI_NUMERICSERV);
-        clients[fd_c].set_response(":wesuseARCH.com NOTICE * :Please authenticate with PASS, then set your NICK and USER.\r\n");
+  char host[NI_MAXHOST];
+  char service[NI_MAXSERV];
+  getnameinfo((struct sockaddr *)&clients[fd_c].get_info(), sizeof(client_addr),
+              host, NI_MAXHOST, service, NI_MAXSERV,
+              NI_NUMERICHOST | NI_NUMERICSERV);
+  clients[fd_c].set_response(":wesuseARCH.com NOTICE * :Please authenticate "
+                             "with PASS, then set your NICK and USER.\r\n");
 }
 
-void    CoreServer::HandleEvents(int fd, std::string buffer)
-{
-    
-    std::cout << "fd: "<< fd << "buffer" << buffer << std::endl;
+void CoreServer::HandleEvents(int fd, std::string buffer) {
 
+  std::cout << "fd: " << fd << "buffer" << buffer << std::endl;
 }
-void    CoreServer::ReadEvent(int fd)
-{
-    char buffer[1024];
-    int readed = read(fd,buffer,1024);
-    if (readed < 0)
-    {
-        std::cout << "closing connection FD:" << fd << std::endl;
-        close(fd);
-        clients.erase(fd);
-        return ;
-        // continue;
+
+void CoreServer::ReadEvent(int fd) {
+  char buffer[1024];
+  int readed = read(fd, buffer, 1024);
+  if (readed < 0) {
+    std::cout << "closing connection FD:" << fd << std::endl;
+    close(fd);
+    clients.erase(fd);
+    return;
+    // continue;
+  } else {
+    std::string str(buffer);
+    clients[fd].set_response(str);
+  }
+}
+
+void CoreServer::WriteEvent(int fd) {
+  int k = 0;
+  if (!clients[fd].get_response().empty()) {
+    k = write(fd, clients[fd].get_response().c_str(),
+              clients[fd].get_response().length());
+    if (k < 0) {
+      std::cout << "writing error" << std::endl;
+      // return ;
     }
-    else
-    {
-        HandleEvents(fd, std::string(buffer));
-    }
-
-
-}
-
-void    CoreServer::WriteEvent(int fd)
-{
-    int k  = 0;
-    if (!clients[fd].get_response().empty())
-    {
-        k = write (fd,clients[fd].get_response().c_str(),clients[fd].get_response().length());
-        if (k < 0)
-        {
-            std::cout << "writing error" << std::endl;
-            // return ;
-        }
-        clients[fd].get_response().erase();
-     }
+    clients[fd].get_response().erase();
+  }
 }
