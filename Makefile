611f0cad12f160a5dd456db48b30d727d9da32e0
Trafexofive <les5phenix@gmail.com>
2024-12-27 04:18:26 +0100

    latest

--- Makefile
+++ Makefile
@@ -27,7 +27,7 @@ EVENTS = -L/usr/local/lib -I/usr/local/include
 OBJ = ${SRC:%.cpp=${DIR_OBJ}%.o}
 CXX = c++
 DEP = ${OBJ:%.o=%.d}
-CPPFLAGS = -Wall -Wextra -Werror -cpp98 -c ${EVENTS} -I ${DIR_INC}
+CPPFLAGS = -Wall -Wextra -Werror -std=c++98 -c ${EVENTS} -I ${DIR_INC}
 RM = rm -f
 RMDIR = rm -rf
 
--- src/server/server.cpp
+++ src/server/server.cpp
@@ -3,66 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mboutuil <mboutuil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: Trafexofive <mboutuil@student.42.fr>       +#+  +:+       +#+      */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/18 05:18:50 by mboutuil          #+#    #+#             */
-/*   Updated: 2024/12/13 21:09:24 by mboutuil         ###   ########.fr       */
+/*   Created: 2024-12-27 03:16:38 by Trafexofive       #+#    #+#           */
+/*   Updated: 2024-12-27 03:16:38 by Trafexofive      ###   ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include"../../inc/core_server.hpp"
+#include "../../inc/core_server.hpp"
+#include <cstdlib>
 
-void    CoreServer::create_socket()
+void CoreServer::create_socket()
 {
-    ServData._socket = socket(AF_INET,SOCK_STREAM,0);
+    ServData._socket = socket(AF_INET, SOCK_STREAM, 0);
     if (ServData._socket < 0)
     {
-        std::cerr << "socket error"<<std::endl;
+        std::cerr << "socket error" << std::endl;
         exit(1);
     }
-    int flags = fcntl(ServData._socket , F_GETFL,0);
-    fcntl(ServData._socket,F_SETFL,flags | O_NONBLOCK);
+    int flags = fcntl(ServData._socket, F_GETFL, 0);
+    fcntl(ServData._socket, F_SETFL, flags | O_NONBLOCK);
     int optval = 1;
-    setsockopt( ServData._socket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+    setsockopt(ServData._socket, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
     ServData.ServAddr.sin_family = AF_INET;
     ServData.ServAddr.sin_port = htons(ServData.Port);
-   ServData.ServAddr.sin_addr.s_addr = INADDR_ANY;
-   struct pollfd _fd;
-   _fd.fd = ServData._socket;
-   _fd.events = POLLIN;
-   fds.push_back(_fd);
+    ServData.ServAddr.sin_addr.s_addr = INADDR_ANY;
+    struct pollfd _fd;
+    _fd.fd = ServData._socket;
+    _fd.events = POLLIN;
+    fds.push_back(_fd);
 }
 
-void    CoreServer::start_listening()
+void CoreServer::start_listening()
 {
-    if (bind(ServData._socket ,(struct sockaddr *)&ServData.ServAddr,sizeof(ServData.ServAddr))< 0)
+    if (bind(ServData._socket, (struct sockaddr*)&ServData.ServAddr, sizeof(ServData.ServAddr)) < 0)
     {
         std::cout << "error biding the socket" << std::endl;
-        exit (1);
+        exit(1);
     }
-    if (listen(ServData._socket,5) < 0)
+    if (listen(ServData._socket, 5) < 0)
     {
         std::cout << "error" << std::endl;
-        exit (1);
+        exit(1);
     }
-    std::cout << "start listening on port:"<< ServData.Port << "..." << std::endl;
+    std::cout << "start listening on port:" << ServData.Port << "..." << std::endl;
 }
 
-void    CoreServer::start_server()
+void CoreServer::start_server()
 {
-    // bzero(fds,sizeof(fds));
-    // fds[0].fd = ServData._socket;
-    // fds[0].events = POLLIN;
-
     while (1)
     {
-        int ret = poll(fds.data(),fds.size(),-1);
+        int ret = poll(&fds[0], fds.size(), -1);
         if (ret < 0)
         {
             std::cout << "poll error" << std::endl;
-            break ;
+            break;
         }
-        for (size_t i = 0;i < fds.size();i++)
+        for (size_t i = 0; i < fds.size(); i++)
         {
             if (fds[i].revents & POLLIN)
             {
@@ -75,93 +72,95 @@ void    CoreServer::start_server()
                     ReadEvent(fds[i].fd);
                 }
             }
-            // else if (fds[i].revents & POLLOUT)
-            // {
-            //     WriteEvent(fds[i].fd);
-
-            // }
         }
     }
 }
 
-
-bool IsValidPort(std::string port ,int& _tport)
+bool isStringDigits(const std::string& str)
 {
-    if (!std::all_of(port.begin(),port.end(),::isdigit))
+    for (std::string::const_iterator it = str.begin(); it != str.end(); ++it)
     {
-        return false;
+        if (!isdigit(*it))
+            return false;
     }
-    // int _tport;
-
-    _tport = std::stoi(port);
-    if (_tport < 1 || _tport > 65535)
-        return false;
     return true;
-    
 }
 
-bool   IsValidPass(std::string _pass,std::string& passwd)
+bool IsValidPort(std::string port, int& _tport)
+{
+    if (!isStringDigits(port))
+        return false;
+
+    char* end;
+    long val = std::strtol(port.c_str(), &end, 10);
+    
+    if (*end != '\0' || val <= 0 || val > 65535)
+        return false;
+        
+    _tport = static_cast<int>(val);
+    return true;
+}
+
+bool IsValidPass(std::string _pass, std::string& passwd)
 {
     if (_pass.length() < 8)
         return false;
-    bool Lower = false,Upper = false,Digit = false ,Special = false;
-        for(size_t i = 0;i < _pass.length();i++)
-        {
-            if (std::islower(_pass[i])) Lower = true;
-            else if (std::isupper(_pass[i])) 
-                Upper = true;
-            else if (std::isdigit(_pass[i]))
-                Digit = true;
-            else if (std::ispunct(_pass[i])) 
-                Special = true;
-        }
-        if ((Lower && Upper && Digit && Special)== false)
-            return  false;
+
+    bool Lower = false, Upper = false, Digit = false, Special = false;
+    
+    for (std::string::iterator it = _pass.begin(); it != _pass.end(); ++it)
+    {
+        if (islower(*it)) Lower = true;
+        else if (isupper(*it)) Upper = true;
+        else if (isdigit(*it)) Digit = true;
+        else if (ispunct(*it)) Special = true;
+    }
+    
+    if (!(Lower && Upper && Digit && Special))
+        return false;
+        
     passwd = _pass;
-    // passwd = polynomialHash(_pass);
     return true;
 }
 
-
-CoreServer::CoreServer(std::string port,std::string password)
+CoreServer::CoreServer(std::string port, std::string password)
 {
-    if (!IsValidPort(port ,ServData.Port))
+    if (!IsValidPort(port, ServData.Port))
     {
         std::cout << "ENTER VALID PORT !!" << std::endl;
-        return ;
+        return;
     }
-    if (!IsValidPass(password,ServData.Passwd))
+    if (!IsValidPass(password, ServData.Passwd))
     {
         std::cout << "ENTER VALID PASSWORD !!" << std::endl;
         std::cout << "At least 1 lowercase letter (a-z) !!" << std::endl;
         std::cout << "At least 1 uppercase letter (A-Z) !!" << std::endl;
         std::cout << "At least 1 digit (0-9) !!" << std::endl;
         std::cout << "At least 1 special character (e.g., !, @, #, $, etc.) !!" << std::endl;
-        return ;
-        // throw "INVALID  PASSWORD";
+        return;
     }
-    commands["NICK"] = &CoreServer::cmdNick;
-    commands["USER"] = &CoreServer::cmdUser;
-    commands["JOIN"] = &CoreServer::cmdJoin;
-    commands["PRIVMSG"] = &CoreServer::cmdPrivmsg;
+    
+    commands[CMD_NICK] = &CoreServer::cmdNick;
+    commands[CMD_USER] = &CoreServer::cmdUser;
+    commands[CMD_JOIN] = &CoreServer::cmdJoin;
+    commands[CMD_PRIVMSG] = &CoreServer::cmdPrivmsg;
+    commands[CMD_PASS] = &CoreServer::cmdPass;  // Added PASS command
+    
     create_socket();
     start_listening();
     start_server();
 }
 
-int main (int ac,char **av)
+int main(int ac, char** av)
 {
-    if (ac != 3 && (!av[1] || !av[2]))
+    if (ac != 3 || !av[1] || !av[2])
     {
         std::cout << "ENTER VALID PARAMETERS !!" << std::endl;
         return 1;
-
     }
+    
     std::string port(av[1]);
     std::string passwd(av[2]);
-        // std::cout << port << std::endl;
-        // exit(1);
-    CoreServer IrcServ(port,passwd);
-
-
+    CoreServer IrcServ(port, passwd);
+    return 0;
 }
